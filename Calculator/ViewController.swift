012f3cf4a9e19ba2965b3cebeb9fb3aadbac86b3
Frederic Deschenes <fredericdeschenes@imac-de-philippe-deschenes.local>
2016-11-26 14:55:46 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Frederic Deschenes on 16-11-26.
+//  Copyright © 2016 Frederic Deschenes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
