becd2f240e167b292e0e38f1d7635e4411f844c1
Ferdzz <fredericdeschenes@imac-de-philippe-deschenes.local>
2016-11-26 22:00:19 -0500

    Finished main UI

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblNumber: UILabel!
+
+
+    var value : String = "0"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        showValue()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    private func showValue() {
+        lblNumber.text = value
+    }
 }
 
